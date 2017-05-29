7ffae5edaa81972cf16106d9470fd691fcc97131
Александр Кухтин <alex.kukhtin@outlook.com>
2017-05-29 22:43:55 +0300

    29 may 2017 22:43

---
+++ Web/A2v10.Web.Site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="A2v10.Web.Site.MvcApplication" Language="C#" %>
---
+++ Web/A2v10.Web.Site/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(A2v10.Web.Site.Startup))]
+namespace A2v10.Web.Site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
