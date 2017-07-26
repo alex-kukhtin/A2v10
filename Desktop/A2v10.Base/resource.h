5e9d6ecacd4f43fb60d79bb16113bc4a67a726cf
Александр Кухтин <alex.kukhtin@outlook.com>
2017-07-26 09:38:53 +0300

    26 jul 2017 9:38

--- Desktop/A2v10.Base/resource.h
+++ Desktop/A2v10.Base/resource.h
@@ -3,17 +3,19 @@
 // Used by A2v10.Base.rc
 //
 
-#define IDIL_10X10					700
-#define IDIL_12X12					701
-#define IDD_OPTION_GENERAL			800
+#define IDIL_10X10				700
+#define IDIL_12X12				701
+#define IDIL_48X48				702
+#define IDD_OPTION_GENERAL		800
+#define IDD_ABOUT				801
 #define IDS_OPTIONS_CAPTION		12000
 #define IDS_LANGUAGES			12001
-#define IDC_DIALOG_FIRST      14000
-#define IDC_COMBO1			  14001
-#define IDC_COMBO2			  14002
-#define IDC_COMBO3			  14003
-#define IDC_COMBO4			  14004
-#define IDC_COMBO5			  14005
+#define IDC_DIALOG_FIRST		14000
+#define IDC_COMBO1				14001
+#define IDC_COMBO2				14002
+#define IDC_COMBO3				14003
+#define IDC_COMBO4				14004
+#define IDC_COMBO5				14005
 #define IDC_TITLE1			  14006
 #define IDC_TITLE2			  14007
 #define IDC_TITLE3			  14008
@@ -23,7 +25,12 @@
 #define IDC_PROMPT2			  14012	
 #define IDC_PROMPT3			  14013	
 #define IDC_PROMPT4			  14014	
-#define IDC_PROMPT5			  14015	
+#define IDC_PROMPT5			  14015
+#define IDC_TEXT1			  14016
+#define IDC_TEXT2			  14017
+#define IDC_TEXT3			  14018
+#define IDC_TEXT4			  14019
+#define IDC_TEXT5			  14020
 
 // Next default values for new objects
 //
