586825f1f72dcacb0a2542754e809afc818dca43
Александр Кухтин <alex.kukhtin@outlook.com>
2018-02-25 22:38:00 +0200

    25 feb 2018 22:37

--- Shared/A2v10.Xaml/Layouts/Container.cs
+++ Shared/A2v10.Xaml/Layouts/Container.cs
@@ -5,47 +5,47 @@ using System.Windows.Markup;
 
 namespace A2v10.Xaml
 {
-    [ContentProperty("Children")]
-    public abstract class Container : UIElement
-    {
-        public UIElementCollection Children { get; set; } = new UIElementCollection();
+	[ContentProperty("Children")]
+	public abstract class Container : UIElement
+	{
+		public UIElementCollection Children { get; set; } = new UIElementCollection();
 
-        public Object ItemsSource { get; set; }
+		public Object ItemsSource { get; set; }
 
-        internal virtual void RenderChildren(RenderContext context, Action<TagBuilder> onRenderStatic = null)
-        {
-            var isBind = GetBinding(nameof(ItemsSource));
-            if (isBind != null)
-            {
-                var tml = new TagBuilder("template");
-                tml.MergeAttribute("v-for", $"(xelem, xIndex) in {isBind.GetPath(context)}");
-                tml.RenderStart(context);
-                using (new ScopeContext(context, "xelem"))
-                {
-                    foreach (var c in Children)
-                    {
-                        c.RenderElement(context, (tag) =>
-                        {
-                            tag.MergeAttribute(":key", "xIndex");
-                        });
-                    }
-                }
-                tml.RenderEnd(context);
-            }
-            else
-            {
-                foreach (var c in Children)
-                {
-                    c.RenderElement(context, onRenderStatic);
-                }
-            }
-        }
+		internal virtual void RenderChildren(RenderContext context, Action<TagBuilder> onRenderStatic = null)
+		{
+			var isBind = GetBinding(nameof(ItemsSource));
+			if (isBind != null)
+			{
+				var tml = new TagBuilder("template");
+				tml.MergeAttribute("v-for", $"(xelem, xIndex) in {isBind.GetPath(context)}");
+				tml.RenderStart(context);
+				using (new ScopeContext(context, "xelem"))
+				{
+					foreach (var c in Children)
+					{
+						c.RenderElement(context, (tag) =>
+						{
+							tag.MergeAttribute(":key", "xIndex");
+						});
+					}
+				}
+				tml.RenderEnd(context);
+			}
+			else
+			{
+				foreach (var c in Children)
+				{
+					c.RenderElement(context, onRenderStatic);
+				}
+			}
+		}
 
-        protected override void OnEndInit()
-        {
-            base.OnEndInit();
-            foreach (var c in Children)
-                c.SetParent(this);
-        }
-    }
+		protected override void OnEndInit()
+		{
+			base.OnEndInit();
+			foreach (var c in Children)
+				c.SetParent(this);
+		}
+	}
 }
