d1f15339eafd5fed2ab5e4cdda00d38cb7f4c180
Александр Кухтин <alex.kukhtin@outlook.com>
2018-03-05 19:22:34 +0200

    05 mar 2018 19:22

--- Shared/A2v10.Xaml/Controls/DatePicker.cs
+++ Shared/A2v10.Xaml/Controls/DatePicker.cs
@@ -5,31 +5,31 @@ using System;
 
 namespace A2v10.Xaml
 {
-    public class DatePicker : ValuedControl
-    {
+	public class DatePicker : ValuedControl, ITableControl
+	{
 
-        public TextAlign Align { get; set; }
+		public TextAlign Align { get; set; }
 
-        internal override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
-        {
-           CheckDisabledModel(context);
-            var input = new TagBuilder("a2-date-picker", null, IsInGrid);
-            if (onRender != null)
-                onRender(input);
-            MergeAttributes(input, context);
-            MergeDisabled(input, context);
-            MergeAlign(input, context, Align);
-            MergeValue(input, context);
-            input.RenderStart(context);
-            RenderAddOns(context);
-            input.RenderEnd(context);
-        }
+		internal override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
+		{
+			CheckDisabledModel(context);
+			var input = new TagBuilder("a2-date-picker", null, IsInGrid);
+			if (onRender != null)
+				onRender(input);
+			MergeAttributes(input, context);
+			MergeDisabled(input, context);
+			MergeAlign(input, context, Align);
+			MergeValue(input, context);
+			input.RenderStart(context);
+			RenderAddOns(context);
+			input.RenderEnd(context);
+		}
 
-        protected override void OnEndInit()
-        {
-            base.OnEndInit();
-            if (Align == TextAlign.Default)
-                Align = TextAlign.Center;
-        }
-    }
+		protected override void OnEndInit()
+		{
+			base.OnEndInit();
+			if (Align == TextAlign.Default)
+				Align = TextAlign.Center;
+		}
+	}
 }
--- Shared/A2v10.Xaml/Controls/TextBox.cs
+++ Shared/A2v10.Xaml/Controls/TextBox.cs
@@ -4,41 +4,41 @@ using System;
 
 namespace A2v10.Xaml
 {
-    public class TextBox : ValuedControl, ITableControl
-    {
-        public String Placeholder { get; set; }
+	public class TextBox : ValuedControl, ITableControl
+	{
+		public String Placeholder { get; set; }
 
-        public TextAlign Align { get; set; }
+		public TextAlign Align { get; set; }
 
-        public Boolean Multiline { get; set; }
+		public Boolean Multiline { get; set; }
 
-        public Int32? Rows { get; set; }
+		public Int32? Rows { get; set; }
 
-        public Boolean Password { get; set; }
+		public Boolean Password { get; set; }
 
-        public Boolean AutoSize { get; set; }
+		public Boolean AutoSize { get; set; }
 
-        internal override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
-        {
-            CheckDisabledModel(context);
-            var tagName = Multiline ? "a2-textarea" : "textbox";
-            var input = new TagBuilder(tagName, null, IsInGrid);
-            if (onRender != null)
-                onRender(input);
-            MergeAttributes(input, context);
-            MergeDisabled(input, context);
-            if (Multiline)
-                MergeAttributeInt32(input, context, "rows", nameof(Rows), Rows);
-            if (Password)
-                input.MergeAttribute(":password", "true");
-            if (AutoSize)
-                input.MergeAttribute(":auto-size", "true");
-            MergeAlign(input, context, Align);
-            MergeBindingAttributeString(input, context, "placeholder", nameof(Placeholder), Placeholder);
-            MergeValue(input, context);
-            input.RenderStart(context);
-            RenderAddOns(context);
-            input.RenderEnd(context);
-        }
-    }
+		internal override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
+		{
+			CheckDisabledModel(context);
+			var tagName = Multiline ? "a2-textarea" : "textbox";
+			var input = new TagBuilder(tagName, null, IsInGrid);
+			if (onRender != null)
+				onRender(input);
+			MergeAttributes(input, context);
+			MergeDisabled(input, context);
+			if (Multiline)
+				MergeAttributeInt32(input, context, "rows", nameof(Rows), Rows);
+			if (Password)
+				input.MergeAttribute(":password", "true");
+			if (AutoSize)
+				input.MergeAttribute(":auto-size", "true");
+			MergeAlign(input, context, Align);
+			MergeBindingAttributeString(input, context, "placeholder", nameof(Placeholder), Placeholder);
+			MergeValue(input, context);
+			input.RenderStart(context);
+			RenderAddOns(context);
+			input.RenderEnd(context);
+		}
+	}
 }
