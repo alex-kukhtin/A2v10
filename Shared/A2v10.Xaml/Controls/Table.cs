5db8db613379ed9c497fb17072ff89ef6eac6825
Александр Кухтин <alex.kukhtin@outlook.com>
2018-03-10 18:58:26 +0200

    10 mar 2018 18:58

--- Shared/A2v10.Xaml/Controls/Table.cs
+++ Shared/A2v10.Xaml/Controls/Table.cs
@@ -7,188 +7,188 @@ using A2v10.Infrastructure;
 namespace A2v10.Xaml
 {
 
-    public enum TableBackgroundStyle
-    {
-        None,
-        Paper,
-        Yellow,
-        Cyan,
-        Rose
-    }
+	public enum TableBackgroundStyle
+	{
+		None,
+		Paper,
+		Yellow,
+		Cyan,
+		Rose
+	}
 
 
-    [ContentProperty("Rows")]
-    public class Table : Control, ITableControl
-    {
-        public GridLinesVisibility GridLines { get; set; }
+	[ContentProperty("Rows")]
+	public class Table : Control, ITableControl
+	{
+		public GridLinesVisibility GridLines { get; set; }
 
-        public TableRowCollection Rows { get; set; } = new TableRowCollection();
+		public TableRowCollection Rows { get; set; } = new TableRowCollection();
 
-        public Boolean Border { get; set; }
-        public Boolean Compact { get; set; }
-        public Boolean Hover { get; set; }
-        public Boolean Striped { get; set; }
+		public Boolean Border { get; set; }
+		public Boolean Compact { get; set; }
+		public Boolean Hover { get; set; }
+		public Boolean Striped { get; set; }
 
-        public TableBackgroundStyle Background { get; set; }
+		public TableBackgroundStyle Background { get; set; }
 
-        public TableRowCollection Header
-        {
-            get
-            {
-                if (_header == null)
-                    _header = new TableRowCollection();
-                return _header;
-            }
-            set
-            {
-                _header = value;
-            }
-        }
+		public TableRowCollection Header
+		{
+			get
+			{
+				if (_header == null)
+					_header = new TableRowCollection();
+				return _header;
+			}
+			set
+			{
+				_header = value;
+			}
+		}
 
-        public TableRowCollection Footer
-        {
-            get
-            {
-                if (_footer == null)
-                    _footer = new TableRowCollection();
-                return _footer;
-            }
-            set { _footer = value; }
-        }
+		public TableRowCollection Footer
+		{
+			get
+			{
+				if (_footer == null)
+					_footer = new TableRowCollection();
+				return _footer;
+			}
+			set { _footer = value; }
+		}
 
-        public TableColumnCollection Columns
-        {
-            get
-            {
-                if (_columns == null)
-                    _columns = new TableColumnCollection();
-                return _columns;
-            }
-            set
-            {
-                _columns = value;
-            }
-        }
+		public TableColumnCollection Columns
+		{
+			get
+			{
+				if (_columns == null)
+					_columns = new TableColumnCollection();
+				return _columns;
+			}
+			set
+			{
+				_columns = value;
+			}
+		}
 
 
-        TableRowCollection _header;
-        TableRowCollection _footer;
-        TableColumnCollection _columns;
+		TableRowCollection _header;
+		TableRowCollection _footer;
+		TableColumnCollection _columns;
 
-        public Object ItemsSource { get; set; }
+		public Object ItemsSource { get; set; }
 
-        internal override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
-        {
-            var table = new TagBuilder("table", "a2-table", IsInGrid);
-            if (onRender != null)
-                onRender(table);
-            MergeAttributes(table, context);
-            if (Background != TableBackgroundStyle.None)
-                table.AddCssClass("bk-" + Background.ToString().ToKebabCase());
+		internal override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
+		{
+			var table = new TagBuilder("table", "a2-table", IsInGrid);
+			if (onRender != null)
+				onRender(table);
+			MergeAttributes(table, context);
+			if (Background != TableBackgroundStyle.None)
+				table.AddCssClass("bk-" + Background.ToString().ToKebabCase());
 
-            if (GridLines != GridLinesVisibility.None)
-                table.AddCssClass($"grid-{GridLines.ToString().ToLowerInvariant()}");
+			if (GridLines != GridLinesVisibility.None)
+				table.AddCssClass($"grid-{GridLines.ToString().ToLowerInvariant()}");
 
-            table.AddCssClassBool(Border, "bordered");
-            table.AddCssClassBool(Compact, "compact");
-            table.AddCssClassBool(Hover, "hover");
-            table.AddCssClassBool(Striped, "striped");
+			table.AddCssClassBool(Border, "bordered");
+			table.AddCssClassBool(Compact, "compact");
+			table.AddCssClassBool(Hover, "hover");
+			table.AddCssClassBool(Striped, "striped");
 
-            Bind isBind = GetBinding(nameof(ItemsSource));
-            if (isBind != null)
-                table.MergeAttribute("v-lazy", isBind.GetPath(context));
+			Bind isBind = GetBinding(nameof(ItemsSource));
+			if (isBind != null)
+				table.MergeAttribute("v-lazy", isBind.GetPath(context));
 
-            table.RenderStart(context);
+			table.RenderStart(context);
 
-            if (_columns != null)
-                Columns.Render(context);
+			if (_columns != null)
+				Columns.Render(context);
 
-            RenderHeader(context);
+			RenderHeader(context);
 
-            RenderBody(context);
-            RenderFooter(context);
+			RenderBody(context);
+			RenderFooter(context);
 
-            table.RenderEnd(context);
-        }
+			table.RenderEnd(context);
+		}
 
-        void RenderHeader(RenderContext context)
-        {
-            if (_header == null)
-                return;
-            var thead = new TagBuilder("thead").RenderStart(context);
-            foreach (var h in Header)
-                h.RenderElement(context);
-            thead.RenderEnd(context);
-        }
-        
-        void RenderBody(RenderContext context)
-        {
-            if (Rows.Count == 0)
-                return;
-            var tbody = new TagBuilder("tbody").RenderStart(context);
-            Bind isBind = GetBinding(nameof(ItemsSource));
-            if (isBind != null)
-            {
-                var repeatAttr = $"(row, rowIndex) in {isBind.GetPath(context)}";
-                using (new ScopeContext(context, "row"))
-                {
-                    if (Rows.Count == 1)
-                    {
-                        Rows[0].RenderElement(context, (tag) =>
-                        {
-                            tag.MergeAttribute("v-for", repeatAttr);
-                        });
-                    }
-                    else
-                    {
-                        var tml = new TagBuilder("template");
-                        tml.MergeAttribute("v-for", repeatAttr);
-                        tml.RenderStart(context);
-                        using (var cts = new ScopeContext(context, "row"))
-                        {
-                            var rNo = 0;
-                            foreach (var row in Rows)
-                            {
-                                row.RenderElement(context, (tag) => tag.MergeAttribute(":key", $"'r{rNo}:' + rowIndex"));
-                                rNo += 1;
-                            }
-                        }
-                        tml.RenderEnd(context);
-                    }
-                }
-            }
-            else
-            {
-                foreach (var row in Rows)
-                    row.RenderElement(context);
-            }
-            tbody.RenderEnd(context);
-        }
-        
-        void RenderFooter(RenderContext context)
-        {
-            if (_footer == null)
-                return;
-            var tfoot = new TagBuilder("tfoot").RenderStart(context);
-            foreach (var f in Footer)
-                f.RenderElement(context);
-            tfoot.RenderEnd(context);
-        }
+		void RenderHeader(RenderContext context)
+		{
+			if (_header == null)
+				return;
+			var thead = new TagBuilder("thead").RenderStart(context);
+			foreach (var h in Header)
+				h.RenderElement(context);
+			thead.RenderEnd(context);
+		}
 
-        protected override void OnEndInit()
-        {
-            base.OnEndInit();
-            foreach (var c in Rows)
-                c.SetParent(this);
-            if (_header != null)
-                foreach (var h in Header)
-                    h.SetParent(this);
-            if (_footer != null)
-                foreach (var f in Footer)
-                    f.SetParent(this);
-            if (_columns != null)
-                foreach (var c in Columns)
-                    c.SetParent(this);
-        }
-    }
+		void RenderBody(RenderContext context)
+		{
+			if (Rows.Count == 0)
+				return;
+			var tbody = new TagBuilder("tbody").RenderStart(context);
+			Bind isBind = GetBinding(nameof(ItemsSource));
+			if (isBind != null)
+			{
+				var repeatAttr = $"(row, rowIndex) in {isBind.GetPath(context)}";
+				using (new ScopeContext(context, "row"))
+				{
+					if (Rows.Count == 1)
+					{
+						Rows[0].RenderElement(context, (tag) =>
+						{
+							tag.MergeAttribute("v-for", repeatAttr);
+						});
+					}
+					else
+					{
+						var tml = new TagBuilder("template");
+						tml.MergeAttribute("v-for", repeatAttr);
+						tml.RenderStart(context);
+						using (var cts = new ScopeContext(context, "row"))
+						{
+							var rNo = 0;
+							foreach (var row in Rows)
+							{
+								row.RenderElement(context, (tag) => tag.MergeAttribute(":key", $"'r{rNo}:' + rowIndex"));
+								rNo += 1;
+							}
+						}
+						tml.RenderEnd(context);
+					}
+				}
+			}
+			else
+			{
+				foreach (var row in Rows)
+					row.RenderElement(context);
+			}
+			tbody.RenderEnd(context);
+		}
+
+		void RenderFooter(RenderContext context)
+		{
+			if (_footer == null)
+				return;
+			var tfoot = new TagBuilder("tfoot").RenderStart(context);
+			foreach (var f in Footer)
+				f.RenderElement(context);
+			tfoot.RenderEnd(context);
+		}
+
+		protected override void OnEndInit()
+		{
+			base.OnEndInit();
+			foreach (var c in Rows)
+				c.SetParent(this);
+			if (_header != null)
+				foreach (var h in Header)
+					h.SetParent(this);
+			if (_footer != null)
+				foreach (var f in Footer)
+					f.SetParent(this);
+			if (_columns != null)
+				foreach (var c in Columns)
+					c.SetParent(this);
+		}
+	}
 }
--- Shared/A2v10.Xaml/Layouts/Panel.cs
+++ Shared/A2v10.Xaml/Layouts/Panel.cs
@@ -1,86 +1,101 @@
 ﻿// Copyright © 2015-2017 Alex Kukhtin. All rights reserved.
 
+using A2v10.Infrastructure;
 using System;
 
 namespace A2v10.Xaml
 {
-    public enum PaneStyle
-    {
-        Default,
-        Info,
-        Warning,
-        Danger,
-        Error,
-        Success,
-        Green,
-        Cyan,
-        Red,
-        Yellow
-    }
+	public enum PaneStyle
+	{
+		Default,
+		Info,
+		Warning,
+		Danger,
+		Error,
+		Success,
+		Green,
+		Cyan,
+		Red,
+		Yellow
+	}
 
-    public class Panel : Container, ITableControl
-    {
-        public Object Header { get; set; }
+	public enum PaneBackgroundStyle
+	{
+		None,
+		LightGray
+	}
 
-        public Boolean Collapsible { get; set; }
 
-        public Boolean? Collapsed { get; set; }
+	public class Panel : Container, ITableControl
+	{
+		public Object Header { get; set; }
 
-        public PaneStyle Style { get; set; }
+		public Boolean Collapsible { get; set; }
 
-        public Icon Icon { get; set; }
+		public Boolean? Collapsed { get; set; }
 
-        internal override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
-        {
-            var panel = new TagBuilder("a2-panel", null, IsInGrid);
-            MergeBindingAttributeBool(panel, context, ":initial-collapsed", nameof(Collapsed), Collapsed);
-            MergeBindingAttributeBool(panel, context, ":collapsible", nameof(Collapsible), Collapsible);
-            if (!HasHeader)
-                panel.MergeAttribute(":no-header", "true");
-            var sb = GetBinding(nameof(Style));
-            if (sb != null)
-                panel.MergeAttribute(":panel-style", sb.GetPathFormat(context));
-            else if (Style != PaneStyle.Default)
-                panel.MergeAttribute("panel-style", Style.ToString().ToLowerInvariant());
-            MergeAttributes(panel, context, MergeAttrMode.Visibility);
-            panel.RenderStart(context);
-            RenderHeader(context);
-            var content = new TagBuilder("div", "panel-content");
-            MergeAttributes(content, context, MergeAttrMode.Margin | MergeAttrMode.Wrap | MergeAttrMode.Tip);
-            content.RenderStart(context);
-            RenderChildren(context);
-            content.RenderEnd(context);
-            panel.RenderEnd(context);
-        }
+		public PaneStyle Style { get; set; }
 
-        Boolean HasHeader => GetBinding(nameof(Header)) != null || Header != null || Icon != Icon.NoIcon;
+		public Icon Icon { get; set; }
 
-        void RenderHeader(RenderContext context)
-        {
-            if (!HasHeader)
-                return;
-            var header = new TagBuilder("div", "panel-header-slot");
-            header.MergeAttribute("slot", "header");
-            header.RenderStart(context);
+		public PaneBackgroundStyle Background { get; set; }
 
-            RenderIcon(context, Icon);
+		public Boolean DropShadow { get; set; }
 
-            var hBind = GetBinding(nameof(Header));
-            if (hBind != null)
-            {
-                var span = new TagBuilder("span");
-                span.MergeAttribute("v-text", hBind.GetPathFormat(context));
-                span.Render(context);
-            }
-            else if (Header is UIElementBase)
-            {
-                (Header as UIElementBase).RenderElement(context);
-            }
-            else if (Header != null)
-            {
-                context.Writer.Write(context.Localize(Header.ToString()));
-            }
-            header.RenderEnd(context);
-        }
-    }
+		internal override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
+		{
+			var panel = new TagBuilder("a2-panel", null, IsInGrid);
+			MergeBindingAttributeBool(panel, context, ":initial-collapsed", nameof(Collapsed), Collapsed);
+			MergeBindingAttributeBool(panel, context, ":collapsible", nameof(Collapsible), Collapsible);
+			if (!HasHeader)
+				panel.MergeAttribute(":no-header", "true");
+			var sb = GetBinding(nameof(Style));
+			if (sb != null)
+				panel.MergeAttribute(":panel-style", sb.GetPathFormat(context));
+			else if (Style != PaneStyle.Default)
+				panel.MergeAttribute("panel-style", Style.ToString().ToLowerInvariant());
+			MergeAttributes(panel, context, MergeAttrMode.Visibility);
+			if (Background != PaneBackgroundStyle.None)
+				panel.AddCssClass("panel-background-" + Background.ToString().ToKebabCase());
+			panel.AddCssClassBool(DropShadow, "drop-shadow");
+			panel.RenderStart(context);
+			RenderHeader(context);
+			var content = new TagBuilder("div", "panel-content");
+			MergeAttributes(content, context, MergeAttrMode.Margin | MergeAttrMode.Wrap | MergeAttrMode.Tip);
+			content.RenderStart(context);
+			RenderChildren(context);
+			content.RenderEnd(context);
+			panel.RenderEnd(context);
+		}
+
+		Boolean HasHeader => GetBinding(nameof(Header)) != null || Header != null || Icon != Icon.NoIcon;
+
+		void RenderHeader(RenderContext context)
+		{
+			if (!HasHeader)
+				return;
+			var header = new TagBuilder("div", "panel-header-slot");
+			header.MergeAttribute("slot", "header");
+			header.RenderStart(context);
+
+			RenderIcon(context, Icon);
+
+			var hBind = GetBinding(nameof(Header));
+			if (hBind != null)
+			{
+				var span = new TagBuilder("span");
+				span.MergeAttribute("v-text", hBind.GetPathFormat(context));
+				span.Render(context);
+			}
+			else if (Header is UIElementBase)
+			{
+				(Header as UIElementBase).RenderElement(context);
+			}
+			else if (Header != null)
+			{
+				context.Writer.Write(context.Localize(Header.ToString()));
+			}
+			header.RenderEnd(context);
+		}
+	}
 }
