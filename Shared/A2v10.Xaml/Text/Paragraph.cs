86b92f8e15897775ac3501d8141c9d3733f3c670
Александр Кухтин <alex.kukhtin@outlook.com>
2018-05-17 19:58:30 +0300

    17 may 2018 19:58

--- Shared/A2v10.Xaml/Text/Paragraph.cs
+++ Shared/A2v10.Xaml/Text/Paragraph.cs
@@ -6,27 +6,20 @@ using System.Windows.Markup;
 namespace A2v10.Xaml
 {
 	[ContentProperty("Inlines")]
-	public class Text : UIElement
+	public class Paragraph : UIElement
 	{
 		public InlineCollection Inlines { get; } = new InlineCollection();
 
-		public TextSize Size { get; set; }
-		public Boolean Gray { get; set; }
 		public Boolean Small { get; set; }
 
 		internal override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
 		{
-			var tag = new TagBuilder("span", null, IsInGrid);
+			var tag = new TagBuilder("p", null, IsInGrid);
 			MergeAttributes(tag, context);
-			if (Size != TextSize.Normal)
-				tag.AddCssClass("text-" + Size.ToString().ToLowerInvariant());
-			tag.AddCssClassBool(Gray, "text-gray");
 			tag.AddCssClassBool(Small, "text-small");
 			tag.RenderStart(context);
 			Inlines.Render(context);
 			tag.RenderEnd(context);
-			//throw new NotImplementedException();
 		}
 	}
 }
-
