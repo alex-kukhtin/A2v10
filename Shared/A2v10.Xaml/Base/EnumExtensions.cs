9448c054fe88599bb1fe7d6a3086693ddade9ca9
Аlex Kukhtin <alex.kukhtin@outlook.com>
2019-01-06 15:06:05 +0200

    v 7403.2

--- Shared/A2v10.Xaml/Base/EnumExtensions.cs
+++ Shared/A2v10.Xaml/Base/EnumExtensions.cs
@@ -6,21 +6,6 @@ namespace A2v10.Xaml
 {
 	public static class EnumExtensions
 	{
-		public static String AlignSelf(this VerticalAlign vAlign)
-		{
-			switch (vAlign)
-			{
-				case VerticalAlign.Top:
-					return "start";
-				case VerticalAlign.Middle:
-					return "center";
-				case VerticalAlign.Bottom:
-					return "end";
-				default:
-					return null;
-			}
-		}
-
 		public static String AlignSelf(this AlignItem vAlign)
 		{
 			switch (vAlign)
