7d01c281d64a5965b3306e85995200f1fd3e0f87
Аlex Kukhtin <alex.kukhtin@outlook.com>
2019-06-21 09:08:20 +0300

    Jasmine tests fixed

--- Web/A2v10.Web.Site/tests/cytools.js
+++ Web/A2v10.Web.Site/tests/cytools.js
@@ -3,7 +3,15 @@
 describe("Currency tools", function () {
 
 	const utils = require('std:utils');
+	const locale = require('std:locale');
+
 	const ct = utils.currency;
+
+	const numLocale = locale.$Locale;
+	const numberFormat = new Intl.NumberFormat(numLocale, { minimumFractionDigits: 2, maximumFractionDigits: 6, useGrouping: true }).format;
+
+	const sep1000 = numberFormat(1000)[1];
+	const sepDec = numberFormat(1000)[5];
 
 	it("round", function () {
 		expect(ct.round(0.833333, 2)).toBe(0.83);
@@ -17,8 +25,9 @@ describe("Currency tools", function () {
 	});
 
 	it("format", function () {
-		expect(ct.format(0)).toBe('0.00');
-		expect(ct.format(248)).toBe('248.00');
+		expect(ct.format(0)).toBe(`0${sepDec}00`);
+		expect(ct.format(248)).toBe(`248${sepDec}00`);
+		expect(ct.format(1248.56)).toBe(`1${sep1000}248${sepDec}56`);
 	});
 
 });
--- Web/A2v10.Web.Site/tests/format.js
+++ Web/A2v10.Web.Site/tests/format.js
@@ -3,27 +3,28 @@
 describe("Format", function () {
 
 	const utils = require('std:utils');
+	const locale = require('std:locale');
 
-	function get1000sep() {
-		return Number(1000).toLocaleString()[1];
-	}
+	const numLocale = locale.$Locale;
+	const numberFormat = new Intl.NumberFormat(numLocale, { minimumFractionDigits: 2, maximumFractionDigits: 6, useGrouping: true }).format;
 
-	let sep = get1000sep();
+	const sep1000 = numberFormat(1000)[1];
+	const sepDec = numberFormat(1000)[5];
 
 
 	it("simple format", function () {
-		expect(utils.format(0.833, "Number")).toBe('0.833');
+		expect(utils.format(0.833, "Number")).toBe('0,833');
 		expect(utils.format(0, "Number")).toBe('0');
-		expect(utils.format(1024, "Number")).toBe(`1${sep}024`);
+		expect(utils.format(1024, "Number")).toBe(`1${sep1000}024`);
 	});
 
 	it("custom format", function () {
-		expect(utils.format(12.3427, "Number", {format:'0.00'})).toBe('12.34');
-		expect(utils.format(12.3427, "Number", { format: '0.00##' })).toBe('12.3427');
+		expect(utils.format(12.3427, "Number", { format: '0.00' })).toBe(`12${sepDec}34`);
+		expect(utils.format(12.3427, "Number", { format: '0.00##' })).toBe(`12${sepDec}3427`);
 		expect(utils.format(12.3427, "Number", { format: '0' })).toBe('12');
-		expect(utils.format(1223.3427, "Number", { format: '#,000.00' })).toBe(`1${sep}223.34`);
+		expect(utils.format(1223.3427, "Number", { format: '#,000.00' })).toBe(`1${sep1000}223${sepDec}34`);
 		expect(utils.format(12, "Number", { format: '000' })).toBe('012');
-		expect(utils.format(1200, "Number", { format: '#,000.00' })).toBe(`1${sep}200.00`);
+		expect(utils.format(1200, "Number", { format: '#,000.00' })).toBe(`1${sep1000}200${sepDec}00`);
 	});
 });
 
