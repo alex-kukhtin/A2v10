6ba1962cb98ecefecee90cb30cb367d88a3c7b2e
Аlex Kukhtin <alex.kukhtin@outlook.com>
2019-10-31 10:52:06 +0200

    v 7574

--- A2v10.Web.Runtime/scripts/main.js
+++ A2v10.Web.Runtime/scripts/main.js
@@ -3902,7 +3902,7 @@ app.modules['std:tools'] = function () {
 
 // Copyright © 2015-2019 Alex Kukhtin. All rights reserved.
 
-// 20190717-7568
+// 20191028-7574
 /* services/html.js */
 
 app.modules['std:html'] = function () {
@@ -3968,7 +3968,7 @@ app.modules['std:html'] = function () {
 	function printDirect(url) {
 
 
-		if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+		if (window.cefHost || navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
 			window.open(url);
 			return;
 		}
