fed55f9fa73e0d514ea7292f92fa335adc4c7d5f
Аlex Kukhtin <alex.kukhtin@outlook.com>
2019-12-04 23:37:44 +0200

    v 7593

--- Tools/BuildApp/Properties/AssemblyInfo.cs
+++ Tools/BuildApp/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("10.0.7592.0")]
-[assembly: AssemblyFileVersion("10.0.7592.0")]
+[assembly: AssemblyVersion("10.0.7593.0")]
+[assembly: AssemblyFileVersion("10.0.7593.0")]
