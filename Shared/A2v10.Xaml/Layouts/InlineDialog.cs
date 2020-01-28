32ab353a67054c6ff21850a12c9707db03966013
Аlex Kukhtin <alex.kukhtin@outlook.com>
2020-01-28 22:06:45 +0200

    inline dialog 1

---
+++ Shared/A2v10.Xaml/Layouts/InlineDialog.cs
@@ -0,0 +1,23 @@
+﻿// Copyright © 2020 Alex Kukhtin. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2v10.Xaml
+{
+	public class InlineDialog : Container
+	{
+		public Length Width { get; set; }
+		public Length MinWidth { get; set; }
+		public Length Height { get; set; }
+
+		public override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
+		{
+			//TODO: How to show it???
+			throw new NotImplementedException();
+		}
+	}
+}
