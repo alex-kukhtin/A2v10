6c3dfdd80f739da5eb5bffb63c6c12f2574f2380
Аlex Kukhtin <alex.kukhtin@outlook.com>
2020-02-06 10:26:17 +0200

    v 7627

--- Tools/BuildSql/Properties/AssemblyInfo.cs
+++ Tools/BuildSql/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("10.0.7626.0")]
-[assembly: AssemblyFileVersion("10.0.7626.0")]
+[assembly: AssemblyVersion("10.0.7627.0")]
+[assembly: AssemblyFileVersion("10.0.7627.0")]
