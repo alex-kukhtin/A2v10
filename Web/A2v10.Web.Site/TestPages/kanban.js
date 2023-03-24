a8c6926b62abab75479131a2622383a510bdc758
alex-kukhtin <alex.kukhtin@outlook.com>
2023-03-24 13:28:43 +0200

    kanban.2

--- Web/A2v10.Web.Site/TestPages/kanban.js
+++ Web/A2v10.Web.Site/TestPages/kanban.js
@@ -6,17 +6,26 @@
 
 let kanbanTemplate = `
 <div class="kanban">
-	<div class=lane v-for="(lane, lx) in lanes" :key=lx>
-		<div class=lane-header>
-			<slot name=header v-bind:lane=lane></slot>
+	<div class="kanban-header kanban-part">
+		<div class=lane-header v-for="(lane, lx) in lanes" :key=lx>
+			<div class=lane-header-body>
+				<slot name=header v-bind:lane=lane></slot>
+			</div>
+			<button>›</button>
 		</div>
-		<ul class=card-list @dragover=dragOver>
-			<li class=card v-for="(card, cx) in cards(lane)" :key=cx :draggable="true"
-				@dragstart="dragStart($event, card)">
-				<slot name=card v-bind:card=card></slot>
-			</li>
-		</ul>
-		<div class=lane-footer>
+	</div>
+	<div class="kanban-body kanban-part">
+		<div class=lane v-for="(lane, lx) in lanes" :key=lx @dragover=dragOver @drop="drop($event, lane)">
+			<ul class=card-list>
+				<li class=card v-for="(card, cx) in cards(lane)" :key=cx :draggable="true"
+						@dragstart="dragStart($event, card)">
+					<slot name=card v-bind:card=card></slot>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="kanban-footer kanban-part">
+		<div class=lane-footer  v-for="(lane, lx) in lanes" :key=lx>
 			<slot name=footer v-bind:lane=lane></slot>
 		</div>
 	</div>
@@ -27,25 +36,34 @@ Vue.component('a2-kanban', {
 	template: kanbanTemplate,
 	props: {
 		lanes: Array,
-		itemsProp: { type: String, default: 'Items' }
+		items: Array,
+		dropDelegate: Function,
+		stateProp: String
 	},
 	data() {
 		return {
+			currentElem: null
 		};
 	},
 	computed: {
 	},
 	methods: {
 		cards(lane) {
-			return lane[this.itemsProp];
+			let id = lane.$id;
+			return this.items.filter(itm => itm[this.stateProp].$id === id);
 		},
 		dragStart(ev, card) {
 			ev.dataTransfer.effectAllowed = "move";
-			console.log(ev, card);
+			this.currentElem = card;
 		},
 		dragOver(ev) {
-			console.dir(ev);
 			ev.preventDefault();
+		},
+		drop(ev, lane) {
+			if (!this.currentElem) return;
+			if (this.dropDelegate)
+				this.dropDelegate(this.currentElem, lane);
+			this.currentElem = null;
 		}
 	}
 });
