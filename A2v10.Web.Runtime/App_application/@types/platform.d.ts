6e93f6969a197d5121b86d9fb0f659009821bdb7
alex-kukhtin <alex.kukhtin@outlook.com>
2023-05-26 08:41:24 +0300

    emitGlobal

--- A2v10.Web.Runtime/App_application/@types/platform.d.ts
+++ A2v10.Web.Runtime/App_application/@types/platform.d.ts
@@ -1,7 +1,7 @@
 ﻿
 /* Copyright © 2019-2023 Oleksandr Kukhtin. All rights reserved. */
 
-/* Version 10.0.7935 */
+/* Version 10.0.7936 */
 
 declare function require(url: string): any;
 
@@ -274,7 +274,7 @@ interface IController {
 	$upload(url: string, accept?: string, data?: { Id?: any, Key?: any }, opts?: { catchError?: boolean }): Promise<any>;
 	$emitCaller(event: string, ...params: any[]): void;
 	$emitSaveEvent(): void;
-	$emitGlobalUpdate(event: string): void;
+	$emitGlobal(event: string, data?: any): void;
 	$emitParentTab(event: string, data?: any): void;
 	$nodirty(func: () => Promise<any>): void;
 	$showSidePane(url: string, arg?: string | number, data?: object): void;
