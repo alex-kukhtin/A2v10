b47413b0bf39672662bb8fcd85503375791e8331
alex-kukhtin <alex.kukhtin@outlook.com>
2023-06-10 09:37:27 +0300

    static maxChars

--- Shared/A2v10.Xaml/Controls/Static.cs
+++ Shared/A2v10.Xaml/Controls/Static.cs
@@ -1,33 +1,34 @@
-﻿// Copyright © 2015-2020 Alex Kukhtin. All rights reserved.
+﻿// Copyright © 2015-2023 Oleksandr Kukhtin. All rights reserved.
 
 using System;
 
-namespace A2v10.Xaml
-{
-	public class Static : ValuedControl, ITableControl
-	{
-		public TextAlign Align { get; set; }
+namespace A2v10.Xaml;
 
-		public override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
+public class Static : ValuedControl, ITableControl
+{
+	public TextAlign Align { get; set; }
+	public UInt32 MaxChars { get; set; }
+
+	public override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
+	{
+		if (SkipRender(context))
+			return;
+		var input = new TagBuilder("static", null, IsInGrid);
+		onRender?.Invoke(input);
+		MergeAttributes(input, context);
+		MergeValue(input, context); // item, prop for validator
+		MergeAlign(input, context, Align);
+		SetSize(input, nameof(Static));
+		var valBind = GetBinding(nameof(Value));
+		if (valBind != null)
 		{
-			if (SkipRender(context))
-				return;
-			var input = new TagBuilder("static", null, IsInGrid);
-			onRender?.Invoke(input);
-			MergeAttributes(input, context);
-			MergeValue(input, context); // item, prop for validator
-			MergeAlign(input, context, Align);
-			SetSize(input, nameof(Static));
-			var valBind = GetBinding(nameof(Value));
-			if (valBind != null)
-			{
-				input.MergeAttribute(":text", valBind.GetPathFormat(context)); // formatted
-				if (valBind.NegativeRed)
-					input.MergeAttribute(":class", $"$getNegativeRedClass({valBind.GetPath(context)})");
-			}
-			input.RenderStart(context);
-			RenderAddOns(context);
-			input.RenderEnd(context);
+			// formatted
+			input.MergeAttribute(":text", MaxChars > 0 ? $"$maxChars({valBind.GetPathFormat(context)}, {MaxChars})" : valBind.GetPathFormat(context));
+			if (valBind.NegativeRed)
+				input.MergeAttribute(":class", $"$getNegativeRedClass({valBind.GetPath(context)})");
 		}
+		input.RenderStart(context);
+		RenderAddOns(context);
+		input.RenderEnd(context);
 	}
 }
