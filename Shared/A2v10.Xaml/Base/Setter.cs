cf92d3b4710d020ce81282bd08834bad8ce79b53
alex-kukhtin <alex.kukhtin@outlook.com>
2023-08-18 06:45:16 +0300

    setter/page

--- Shared/A2v10.Xaml/Base/Setter.cs
+++ Shared/A2v10.Xaml/Base/Setter.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2015-2020 Alex Kukhtin. All rights reserved.
+﻿// Copyright © 2015-2023 Oleksandr Kukhtin. All rights reserved.
 
 using System;
 using System.Collections.Generic;
@@ -7,49 +7,53 @@ using System.Reflection;
 
 using A2v10.Infrastructure;
 
-namespace A2v10.Xaml
-{
-	public class Setter : XamlElement
-	{
-		public String Property { get; set; }
-		public Object Value { get; set; }
+namespace A2v10.Xaml;
 
-		public void SetProperty(XamlElement elem, IDictionary<String, PropertyInfo> props)
+public class Setter : XamlElement
+{
+	public String Property { get; set; }
+	public Object Value { get; set; }
+
+	public void SetProperty(XamlElement elem, IDictionary<String, PropertyInfo> props)
+	{
+		if (!props.TryGetValue(Property, out PropertyInfo prop))
+			throw new XamlException($"The property '{Property}' not found in '{elem.GetType().Name}'");
+		var valBind = GetBinding(nameof(Value));
+		if (valBind != null)
 		{
-			if (!props.TryGetValue(Property, out PropertyInfo prop))
-				throw new XamlException($"The property '{Property}' not found in '{elem.GetType().Name}'");
-			var valBind = GetBinding(nameof(Value));
-			if (valBind != null)
-			{
-				elem.SetBinding(Property, valBind);
-				return;
-			}
-			if (Value == null)
-				return;
-			if (prop.PropertyType.IsEnum)
-			{
-				var converter = TypeDescriptor.GetConverter(prop.PropertyType);
-				var enumVal = converter.ConvertFromString(Value.ToString());
-				prop.SetValue(elem, enumVal);
-			}
-			else if (prop.PropertyType.IsClass)
+			elem.SetBinding(Property, valBind);
+			return;
+		}
+		if (Value == null)
+			return;
+		if (prop.PropertyType.IsEnum)
+		{
+            var oldVal = prop.GetValue(elem);
+            if (oldVal != null && (Int32)oldVal != 0)
+                return;
+            var converter = TypeDescriptor.GetConverter(prop.PropertyType);
+			var enumVal = converter.ConvertFromString(Value.ToString());
+			prop.SetValue(elem, enumVal);
+		}
+		else if (prop.PropertyType.IsClass)
             {
-				var converter = TypeDescriptor.GetConverter(prop.PropertyType);
-				if (converter == null)
-					throw new XamlException($"Could not find converter for '{prop.PropertyType}'");
-				var classVal = converter.ConvertFromString(Value.ToString());
-				prop.SetValue(elem, classVal);
-			}
-			else
+            var oldVal = prop.GetValue(elem);
+            if (oldVal != null)
+                return;
+            var converter = TypeDescriptor.GetConverter(prop.PropertyType)
+				?? throw new XamlException($"Could not find converter for '{prop.PropertyType}'");
+			var classVal = converter.ConvertFromString(Value.ToString());
+			prop.SetValue(elem, classVal);
+		}
+		else
+		{
+			var propType = prop.PropertyType;
+			if (propType.IsNullableType())
 			{
-				var propType = prop.PropertyType;
-				if (propType.IsNullableType())
-				{
-					propType = Nullable.GetUnderlyingType(propType);
-				}
-				var val = Convert.ChangeType(Value, propType);
-				prop.SetValue(elem, val);
+				propType = Nullable.GetUnderlyingType(propType);
 			}
+			var val = Convert.ChangeType(Value, propType);
+			prop.SetValue(elem, val);
 		}
 	}
 }
--- Shared/A2v10.Xaml/Layouts/Page.cs
+++ Shared/A2v10.Xaml/Layouts/Page.cs
@@ -1,193 +1,202 @@
-﻿// Copyright © 2015-2022 Alex Kukhtin. All rights reserved.
+﻿// Copyright © 2015-2023 Oleksandr Kukhtin. All rights reserved.
 
 using System;
 using System.Windows.Markup;
 
 using A2v10.Infrastructure;
 
-namespace A2v10.Xaml
+namespace A2v10.Xaml;
+
+[ContentProperty("Children")]
+public class Page : RootContainer
 {
-	[ContentProperty("Children")]
-	public class Page : RootContainer
+
+	public UIElementBase Toolbar { get; set; }
+	public UIElementBase Taskpad { get; set; }
+	public Pager Pager { get; set; }
+	public String Title { get; set; }
+	//public Double Zoom { get; set; }
+
+	public PrintPage PrintPage { get; set; }
+
+	public BackgroundStyle Background { get; set; }
+	public CollectionView CollectionView { get; set; }
+
+	public override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
 	{
+		if (SkipRender(context))
+			return;
+		TagBuilder page = null;
+		Boolean isGridPage = (Toolbar != null) || (Taskpad != null) || (Pager != null);
 
-		public UIElementBase Toolbar { get; set; }
-		public UIElementBase Taskpad { get; set; }
-		public Pager Pager { get; set; }
-		public String Title { get; set; }
-		//public Double Zoom { get; set; }
+		// render page OR colleciton view
 
-		public PrintPage PrintPage { get; set; }
-
-		public BackgroundStyle Background { get; set; }
-		public CollectionView CollectionView { get; set; }
-
-		public override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
+		void addGridAction(TagBuilder tag)
 		{
-			if (SkipRender(context))
+			if (!isGridPage)
 				return;
-			TagBuilder page = null;
-			Boolean isGridPage = (Toolbar != null) || (Taskpad != null) || (Pager != null);
+			tag.AddCssClass("page-grid");
+			var tp = GetTaskpad();
 
-			// render page OR colleciton view
-
-			void addGridAction(TagBuilder tag)
-			{
-				if (!isGridPage)
-					return;
-				tag.AddCssClass("page-grid");
-				if (Taskpad != null)
+            if (tp != null && tp.Width != null)
+            {
+                if (tp.Position == TaskpadPosition.Left)
 				{
-					if (Taskpad is Taskpad tp && tp.Width != null)
-					{
-						if (tp.Position == TaskpadPosition.Left)
-						{
-							tag.AddCssClass("taskpad-left");
-							tag.MergeStyle("grid-template-columns", $"{tp.Width.Value} 1fr");
-						}
-						else
-							tag.MergeStyle("grid-template-columns", $"1fr {tp.Width.Value}");
-					}
+					tag.AddCssClass("taskpad-left");
+					tag.MergeStyle("grid-template-columns", $"{tp.Width.Value} 1fr");
 				}
-			}
-
-			if (CollectionView != null)
-			{
-				CollectionView.RenderStart(context, (tag) =>
-				{
-					tag.AddCssClass("page").AddCssClass("absolute");
-					addGridAction(tag);
-					AddAttributes(tag);
-					tag.MergeAttribute("id", context.RootId);
-				});
-			}
-			else
-			{
-				page = new TagBuilder("div", "page absolute");
-				page.MergeAttribute("id", context.RootId);
-				addGridAction(page);
-				AddAttributes(page);
-				if (!isGridPage)
-					MergeAttributes(page, context, MergeAttrMode.Margin);
-
-				if (PrintPage != null)
-				{
-					page.AddCssClass("print-target");
-					page.AddCssClass(PrintPage.Orientation.ToString().ToLowerInvariant());
-					page.MergeAttribute("v-print-page", PrintPage.ToJson());
-				}
-
-				page.RenderStart(context);
-			}
-
-
-			RenderTitle(context);
-
-			if (isGridPage)
-			{
-				Toolbar?.RenderElement(context, (tag) => tag.AddCssClass("page-toolbar"));
-				Taskpad?.RenderElement(context, (tag) => tag.AddCssClass("page-taskpad"));
-				Pager?.RenderElement(context, (tag) => tag.AddCssClass("page-pager"));
-				var content = new TagBuilder("div", "page-content");
-				if (ChildHasWrapper)
-					content.AddCssClass("with-wrapper");
-				MergeAttributes(content, context, MergeAttrMode.Margin);
-				content.RenderStart(context);
-				RenderChildren(context);
-				content.RenderEnd(context);
-			}
-			else
-				RenderChildren(context);
-
-			var outer = new TagBuilder("div", "page-canvas-outer").RenderStart(context);
-			new TagBuilder("div", "page-canvas").MergeAttribute("id", "page-canvas").Render(context);
-			outer.RenderEnd(context);
-
-			RenderContextMenus();
-			RenderAccelCommands(context);
-
-			if (CollectionView != null)
-				CollectionView.RenderEnd(context);
-			else
-			{
-				if (page == null)
-					throw new InvalidProgramException();
-				page.RenderEnd(context);
+				else
+					tag.MergeStyle("grid-template-columns", $"1fr {tp.Width.Value}");
 			}
 		}
 
-		public Boolean ChildHasWrapper => Children != null && Children.Count == 1 && Children[0] is IHasWrapper;
-
-		void AddAttributes(TagBuilder tag)
+		if (CollectionView != null)
 		{
-			if (Background != BackgroundStyle.Default)
-				tag.AddCssClass("background-" + Background.ToString().ToKebabCase());
-			tag.AddCssClass(CssClass);
-			tag.AddCssClassBoolNo(UserSelect, "user-select");
-			if (Absolute != null)
+			CollectionView.RenderStart(context, (tag) =>
 			{
-				Absolute.MergeAbsolute(tag);
-				tag.MergeStyle("width", "auto");
-				tag.MergeStyle("height", "auto");
+				tag.AddCssClass("page").AddCssClass("absolute");
+				addGridAction(tag);
+				AddAttributes(tag);
+				tag.MergeAttribute("id", context.RootId);
+			});
+		}
+		else
+		{
+			page = new TagBuilder("div", "page absolute");
+			page.MergeAttribute("id", context.RootId);
+			addGridAction(page);
+			AddAttributes(page);
+			if (!isGridPage)
+				MergeAttributes(page, context, MergeAttrMode.Margin);
+
+			if (PrintPage != null)
+			{
+				page.AddCssClass("print-target");
+				page.AddCssClass(PrintPage.Orientation.ToString().ToLowerInvariant());
+				page.MergeAttribute("v-print-page", PrintPage.ToJson());
 			}
 
-			/*
-			if (Zoom != 0.0)
-				tag.MergeStyle("zoom", Zoom.ToString());
-			*/
+			page.RenderStart(context);
 		}
 
-		void RenderTitle(RenderContext context)
+
+		RenderTitle(context);
+
+		if (isGridPage)
 		{
-			Bind titleBind = GetBinding(nameof(Title));
-			if (titleBind != null || !String.IsNullOrEmpty(Title))
-			{
-				var dt = new TagBuilder("a2-document-title");
-				MergeBindingAttributeString(dt, context, "page-title", nameof(Title), Title);
-				dt.Render(context);
-			}
+			Toolbar?.RenderElement(context, (tag) => tag.AddCssClass("page-toolbar"));
+			Taskpad?.RenderElement(context, (tag) => tag.AddCssClass("page-taskpad"));
+			Pager?.RenderElement(context, (tag) => tag.AddCssClass("page-pager"));
+			var content = new TagBuilder("div", "page-content");
+			if (ChildHasWrapper)
+				content.AddCssClass("with-wrapper");
+			MergeAttributes(content, context, MergeAttrMode.Margin);
+			content.RenderStart(context);
+			RenderChildren(context);
+			content.RenderEnd(context);
+		}
+		else
+			RenderChildren(context);
+
+		var outer = new TagBuilder("div", "page-canvas-outer").RenderStart(context);
+		new TagBuilder("div", "page-canvas").MergeAttribute("id", "page-canvas").Render(context);
+		outer.RenderEnd(context);
+
+		RenderContextMenus();
+		RenderAccelCommands(context);
+
+		if (CollectionView != null)
+			CollectionView.RenderEnd(context);
+		else
+		{
+			if (page == null)
+				throw new InvalidProgramException();
+			page.RenderEnd(context);
+		}
+	}
+
+	public Boolean ChildHasWrapper => Children != null && Children.Count == 1 && Children[0] is IHasWrapper;
+
+    private Taskpad GetTaskpad()
+    {
+        if (Taskpad == null)
+            return null;
+        if (Taskpad is Taskpad tp)
+            return tp;
+        if (Taskpad is ContentControl cc && cc?.Content is Taskpad tpc)
+            return tpc;
+        return null;
+    }
+
+    void AddAttributes(TagBuilder tag)
+	{
+		if (Background != BackgroundStyle.Default)
+			tag.AddCssClass("background-" + Background.ToString().ToKebabCase());
+		tag.AddCssClass(CssClass);
+		tag.AddCssClassBoolNo(UserSelect, "user-select");
+		if (Absolute != null)
+		{
+			Absolute.MergeAbsolute(tag);
+			tag.MergeStyle("width", "auto");
+			tag.MergeStyle("height", "auto");
 		}
 
-		protected override void OnEndInit()
-		{
-			base.OnEndInit();
-			Toolbar?.SetParent(this);
-			Taskpad?.SetParent(this);
-			Pager?.SetParent(this);
-			CollectionView?.SetParent(this);
-		}
+		/*
+		if (Zoom != 0.0)
+			tag.MergeStyle("zoom", Zoom.ToString());
+		*/
+	}
 
-		public override void OnSetStyles(RootContainer root)
+	void RenderTitle(RenderContext context)
+	{
+		Bind titleBind = GetBinding(nameof(Title));
+		if (titleBind != null || !String.IsNullOrEmpty(Title))
 		{
-			base.OnSetStyles(root);
-			Toolbar?.OnSetStyles(root);
-			Taskpad?.OnSetStyles(root);
-			Pager?.OnSetStyles(root);
-			CollectionView?.OnSetStyles(root);
+			var dt = new TagBuilder("a2-document-title");
+			MergeBindingAttributeString(dt, context, "page-title", nameof(Title), Title);
+			dt.Render(context);
 		}
+	}
 
-		public override void OnDispose()
-		{
-			base.OnDispose();
-			Toolbar?.OnDispose();
-			Taskpad?.OnDispose();
-			Pager?.OnDispose();
-			CollectionView?.OnDispose();
-		}
+	protected override void OnEndInit()
+	{
+		base.OnEndInit();
+		Toolbar?.SetParent(this);
+		Taskpad?.SetParent(this);
+		Pager?.SetParent(this);
+		CollectionView?.SetParent(this);
+	}
 
-		protected override T FindInside<T>()
-		{
-			if (this is T elemT)
-				return elemT;
-			else if (Toolbar is T toolbar)
-				return toolbar;
-			else if (CollectionView is T collectionView)
-				return collectionView;
-			else if (Taskpad is T taskpad)
-				return taskpad;
-			else if (Pager is T pager)
-				return pager;
-			return null;
-		}
+	public override void OnSetStyles(RootContainer root)
+	{
+		base.OnSetStyles(root);
+		Toolbar?.OnSetStyles(root);
+		Taskpad?.OnSetStyles(root);
+		Pager?.OnSetStyles(root);
+		CollectionView?.OnSetStyles(root);
+	}
+
+	public override void OnDispose()
+	{
+		base.OnDispose();
+		Toolbar?.OnDispose();
+		Taskpad?.OnDispose();
+		Pager?.OnDispose();
+		CollectionView?.OnDispose();
+	}
+
+	protected override T FindInside<T>()
+	{
+		if (this is T elemT)
+			return elemT;
+		else if (Toolbar is T toolbar)
+			return toolbar;
+		else if (CollectionView is T collectionView)
+			return collectionView;
+		else if (Taskpad is T taskpad)
+			return taskpad;
+		else if (Pager is T pager)
+			return pager;
+		return null;
 	}
 }
