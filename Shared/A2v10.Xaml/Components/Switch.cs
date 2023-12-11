8d61ecab5798582fa79daad9fb67eeeb1359a0f0
alex-kukhtin <alex.kukhtin@outlook.com>
2023-12-11 17:53:15 +0200

    javascript update

--- Shared/A2v10.Xaml/Components/Switch.cs
+++ Shared/A2v10.Xaml/Components/Switch.cs
@@ -1,128 +1,127 @@
-﻿// Copyright © 2015-2021 Alex Kukhtin. All rights reserved.
+﻿// Copyright © 2015-2023 Oleksandr Kukhtin. All rights reserved.
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Markup;
 
-namespace A2v10.Xaml
+namespace A2v10.Xaml;
+
+[ContentProperty("Children")]
+public class Case : XamlElement
 {
-	[ContentProperty("Children")]
-	public class Case : XamlElement
-	{
-		public UIElementCollection Children { get; set; } = new UIElementCollection();
-		public String Value { get; set; }
+    public UIElementCollection Children { get; set; } = new UIElementCollection();
+    public String Value { get; set; }
 
-		public void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
-		{
-			var parentGrid = FindParent<Grid>();
-			if (parentGrid != null)
-			{
-				foreach (var c in Children)
-				{
-					c.IsInGrid = true;
-					using (context.GridContext(c))
-					{
-						c.RenderElement(context);
-					}
-				}
-			}
-			else
-			{
-				foreach (var c in Children)
-				{
-					c.RenderElement(context);
-				}
-			}
-		}
-
-        public override void OnSetStyles(RootContainer root)
+    public void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
+    {
+        var parentGrid = FindParent<Grid>();
+        if (parentGrid != null)
         {
-            base.OnSetStyles(root);
-			foreach (var c in Children)
-				c.OnSetStyles(root);
-		}
-
-        protected override void OnEndInit()
-        {
-            base.OnEndInit();
-			foreach (var c in Children)
-				c.SetParent(this);
-		}
-
-        public override void OnDispose()
-        {
-            base.OnDispose();
-			foreach (var c in Children)
-				c.OnDispose();
-		}
-	}
-
-	public class Else : Case
-	{
-	}
-
-	public class CaseCollection : List<Case>
-	{
-		public CaseCollection()
-		{
-		}
-	}
-
-	[ContentProperty("Cases")]
-	public class Switch : UIElementBase
-	{
-		public Object Expression { get; set; }
-
-		public CaseCollection Cases { get; set; } = new CaseCollection();
-
-		public override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
-		{
-			var expr = GetBinding(nameof(Expression));
-			if (expr == null)
-				throw new XamlException("Binding 'Expression' must be a Bind");
-
-			var cases = Cases.OrderBy(x => x is Else).ToList();
-
-			for (var i=0; i<cases.Count; i++)
-			{
-				var itm = cases[i];
-				var t = new TagBuilder("template", null);
-				var ifKey = (i == 0) ? "v-if " : "v-else-if";
-				if (itm is Else)
-				{
-					t.MergeAttribute("v-else", String.Empty);
-				}
-				else
-				{
-					// convert values to string!
-					t.MergeAttribute(ifKey, $"('' + {expr.GetPathFormat(context)}) === '{itm.Value}'");
-				}
-				t.RenderStart(context);
-				itm.RenderElement(context);
-				t.RenderEnd(context);
-			}
-		}
-
-        public override void OnSetStyles(RootContainer root)
-        {
-            base.OnSetStyles(root);
-			foreach (var c in Cases)
-				c.OnSetStyles(root);
+            foreach (var c in Children)
+            {
+                c.IsInGrid = true;
+                using (context.GridContext(c))
+                {
+                    c.RenderElement(context);
+                }
+            }
         }
-
-        protected override void OnEndInit()
+        else
         {
-            base.OnEndInit();
-			foreach (var c in Cases)
-				c.SetParent(this);
-		}
+            foreach (var c in Children)
+            {
+                c.RenderElement(context);
+            }
+        }
+    }
 
-        public override void OnDispose()
+    public override void OnSetStyles(RootContainer root)
+    {
+        base.OnSetStyles(root);
+        foreach (var c in Children)
+            c.OnSetStyles(root);
+    }
+
+    protected override void OnEndInit()
+    {
+        base.OnEndInit();
+        foreach (var c in Children)
+            c.SetParent(this);
+    }
+
+    public override void OnDispose()
+    {
+        base.OnDispose();
+        foreach (var c in Children)
+            c.OnDispose();
+    }
+}
+
+public class Else : Case
+{
+}
+
+public class CaseCollection : List<Case>
+{
+    public CaseCollection()
+    {
+    }
+}
+
+[ContentProperty("Cases")]
+public class Switch : UIElementBase
+{
+    public Object Expression { get; set; }
+
+    public CaseCollection Cases { get; set; } = new CaseCollection();
+
+    public override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
+    {
+        var expr = GetBinding(nameof(Expression));
+        if (expr == null)
+            throw new XamlException("Binding 'Expression' must be a Bind");
+
+        var cases = Cases.OrderBy(x => x is Else).ToList();
+
+        for (var i = 0; i < cases.Count; i++)
         {
-            base.OnDispose();
-			foreach (var c in Cases)
-				c.OnDispose();
-		}
-	}
+            var itm = cases[i];
+            var t = new TagBuilder("template", null);
+            var ifKey = (i == 0) ? "v-if " : "v-else-if";
+            if (itm is Else)
+            {
+                t.MergeAttribute("v-else", String.Empty);
+            }
+            else
+            {
+                // convert values to string!
+                t.MergeAttribute(ifKey, $"('' + {expr.GetPathFormat(context)}) === '{itm.Value}'");
+            }
+            t.RenderStart(context);
+            itm.RenderElement(context);
+            t.RenderEnd(context);
+        }
+    }
+
+    public override void OnSetStyles(RootContainer root)
+    {
+        base.OnSetStyles(root);
+        foreach (var c in Cases)
+            c.OnSetStyles(root);
+    }
+
+    protected override void OnEndInit()
+    {
+        base.OnEndInit();
+        foreach (var c in Cases)
+            c.SetParent(this);
+    }
+
+    public override void OnDispose()
+    {
+        base.OnDispose();
+        foreach (var c in Cases)
+            c.OnDispose();
+    }
 }
