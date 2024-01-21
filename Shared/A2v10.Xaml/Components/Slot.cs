6ddf77c66f13fba4b21cf446eb3467448229d0d8
alex-kukhtin <alex.kukhtin@outlook.com>
2024-01-21 11:35:16 +0200

    v 7958.2

--- Shared/A2v10.Xaml/Components/Slot.cs
+++ Shared/A2v10.Xaml/Components/Slot.cs
@@ -1,51 +1,74 @@
-﻿// Copyright © 2015-2021 Alex Kukhtin. All rights reserved.
+﻿// Copyright © 2015-2024 Oleksandr Kukhtin. All rights reserved.
 
 using System;
 using System.Windows.Markup;
 
-namespace A2v10.Xaml
+namespace A2v10.Xaml;
+
+[ContentProperty("Children")]
+public class Slot : UIElementBase
 {
-	[ContentProperty("Children")]
-	public class Slot : UIElementBase
+	const String SLOT_ITEM = "__si__";
+
+	public Object Scope { get; set; }
+	public UIElementBase Fallback { get; set; }
+	public UIElementCollection Children { get; set; } = new UIElementCollection();
+
+	public override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
 	{
-		const String SLOT_ITEM = "__si__";
+		if (SkipRender(context))
+			return;
+		var scopeBind = GetBinding(nameof(Scope));
+		if (scopeBind == null)
+			throw new XamlException("Scope must be specified for Slot component");
 
-		public Object Scope { get; set; }
-		public UIElementBase Fallback { get; set; }
-		public UIElementCollection Children { get; set; } = new UIElementCollection();
+		String slotItem = $"{SLOT_ITEM}{context.ScopeLevel}";
 
-		public override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
+		var tag = new TagBuilder("template", null, IsInGrid);
+		tag.MergeAttribute("v-if", $"!!{scopeBind.GetPathFormat(context)}");
+		tag.MergeAttribute("v-for", $"{slotItem} in [{scopeBind.GetPath(context)}]");
+
+		tag.RenderStart(context);
+		using (var ctx = new ScopeContext(context, slotItem, scopeBind.Path))
 		{
-			if (SkipRender(context))
-				return;
-			var scopeBind = GetBinding(nameof(Scope));
-			if (scopeBind == null)
-				throw new XamlException("Scope must be specified for Slot component");
-
-			String slotItem = $"{SLOT_ITEM}{context.ScopeLevel}";
-
-			var tag = new TagBuilder("template", null, IsInGrid);
-			tag.MergeAttribute("v-if", $"!!{scopeBind.GetPathFormat(context)}");
-			tag.MergeAttribute("v-for", $"{slotItem} in [{scopeBind.GetPath(context)}]");
-
-			tag.RenderStart(context);
-			using (var ctx = new ScopeContext(context, slotItem, scopeBind.Path))
+			foreach (var c in Children)
 			{
-				foreach (var c in Children)
-				{
-					c.IsInGrid = IsInGrid;
-					c.RenderElement(context);
-				}
-			}
-			tag.RenderEnd(context);
-			if (Fallback != null)
-			{
-				var fb = new TagBuilder("template");
-				fb.MergeAttribute("v-if", $"!{scopeBind.GetPathFormat(context)}");
-				fb.RenderStart(context);
-				Fallback.RenderElement(context);
-				fb.RenderEnd(context);
+				c.IsInGrid = IsInGrid;
+				c.RenderElement(context);
 			}
 		}
+		tag.RenderEnd(context);
+		if (Fallback != null)
+		{
+			var fb = new TagBuilder("template");
+			fb.MergeAttribute("v-if", $"!{scopeBind.GetPathFormat(context)}");
+			fb.RenderStart(context);
+			Fallback.RenderElement(context);
+			fb.RenderEnd(context);
+		}
+	}
+
+	public override void OnSetStyles(RootContainer root)
+	{
+		base.OnSetStyles(root);
+		foreach (var c in Children)
+			c.OnSetStyles(root);
+		Fallback?.OnSetStyles(root);
+	}
+
+	protected override void OnEndInit()
+	{
+		base.OnEndInit();
+		foreach (var c in Children)
+			c.SetParent(this);
+		Fallback?.SetParent(this);	
+	}
+
+	public override void OnDispose()
+	{
+		base.OnDispose();
+		foreach (var c in Children)
+			c.OnDispose();
+		Fallback?.OnDispose();	
 	}
 }
