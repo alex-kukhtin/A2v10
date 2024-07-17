81d222121e1bf2dcdb851166358e4adca8ec8059
alex-kukhtin <alex.kukhtin@outlook.com>
2024-07-17 06:17:17 +0300

    tabbar fix

--- Shared/A2v10.Xaml/Layouts/TabBar.cs
+++ Shared/A2v10.Xaml/Layouts/TabBar.cs
@@ -1,108 +1,114 @@
-﻿// Copyright © 2019 Alex Kukhtin. All rights reserved.
+﻿// Copyright © 2019-2024 Oleksandr Kukhtin. All rights reserved.
 
 
 using A2v10.Infrastructure;
 using System;
 using System.Windows.Markup;
 
-namespace A2v10.Xaml
-{
+namespace A2v10.Xaml;
 
-	public enum TabBarStyle
+
+public enum TabBarStyle
+{
+	Default,
+	MainMenu,
+	Tab,
+	Wizard,
+	ButtonGroup
+}
+
+[ContentProperty("Buttons")]
+public class TabBar : UIElement
+{
+	public TabButtonCollection Buttons { get; set; } = new TabButtonCollection();
+	public Object Value { get; set; }
+	public ShadowStyle DropShadow { get; set; }
+	public Object Description { get; set; }
+
+	public Object ItemsSource { get; set; }
+	public TabBarStyle Style { get; set; }
+
+	public override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
 	{
-		Default,
-		MainMenu,
-		Tab,
-		Wizard,
-		ButtonGroup
+		if (SkipRender(context))
+			return;
+		var panel = new TagBuilder("div", "a2-tab-bar", IsInGrid);
+		onRender?.Invoke(panel);
+		MergeAttributes(panel, context);
+		if (DropShadow != ShadowStyle.None)
+		{
+			panel.AddCssClass("drop-shadow");
+			panel.AddCssClass(DropShadow.ToString().ToLowerInvariant());
+		}
+		if (Style != TabBarStyle.Default)
+			panel.AddCssClass($"tab-bar-{Style.ToString().ToKebabCase()}");
+
+		panel.RenderStart(context);
+		RenderButtons(context);
+		if (HasDescription)
+			RenderDesription(context);
+		panel.RenderEnd(context);
 	}
 
-	[ContentProperty("Buttons")]
-	public class TabBar : UIElement
+	Boolean HasDescription => Description != null || GetBinding(nameof(Description)) != null;
+
+	void RenderButtons(RenderContext context)
 	{
-		public TabButtonCollection Buttons { get; set; } = new TabButtonCollection();
-		public Object Value { get; set; }
-		public ShadowStyle DropShadow { get; set; }
-		public Object Description { get; set; }
+		var isBind = GetBinding(nameof(ItemsSource));
+		if (isBind != null && Buttons.Count != 1)
+			throw new XamlException("For a TabBar with an items source, only one child element is allowed");
 
-		public Object ItemsSource { get; set; }
-		public TabBarStyle Style { get; set; }
-
-		public override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
+		var valBind = GetBinding(nameof(Value));
+		String valPath = valBind?.GetPathFormat(context);
+		foreach (var b in Buttons)
 		{
-			if (SkipRender(context))
-				return;
-			var panel = new TagBuilder("div", "a2-tab-bar", IsInGrid);
-			onRender?.Invoke(panel);
-			MergeAttributes(panel, context);
-			if (DropShadow != ShadowStyle.None)
+			var tag = new TagBuilder(null, "a2-tab-bar-item");
+			b.MergeAttributes(tag, context, MergeAttrMode.Visibility);
+			if (isBind != null)
 			{
-				panel.AddCssClass("drop-shadow");
-				panel.AddCssClass(DropShadow.ToString().ToLowerInvariant());
-			}
-			if (Style != TabBarStyle.Default)
-				panel.AddCssClass($"tab-bar-{Style.ToString().ToKebabCase()}");
-
-			panel.RenderStart(context);
-			RenderButtons(context);
-			if (HasDescription)
-				RenderDesription(context);
-			panel.RenderEnd(context);
-		}
-
-		Boolean HasDescription => Description != null || GetBinding(nameof(Description)) != null;
-
-		void RenderButtons(RenderContext context)
-		{
-			var isBind = GetBinding(nameof(ItemsSource));
-			if (isBind != null && Buttons.Count != 1)
-				throw new XamlException("For a TabBar with an items source, only one child element is allowed");
-
-			var valBind = GetBinding(nameof(Value));
-			String valPath = valBind?.GetPathFormat(context);
-			foreach (var b in Buttons)
-			{
-				var tag = new TagBuilder(null, "a2-tab-bar-item");
-				b.MergeAttributes(tag, context, MergeAttrMode.Visibility);
-				if (isBind != null)
+				tag.MergeAttribute("v-for", $"(btn, btnIndex) in {isBind.GetPath(context)}");
+				using (new ScopeContext(context, "btn", isBind.Path))
 				{
-					tag.MergeAttribute("v-for", $"(btn, btnIndex) in {isBind.GetPath(context)}");
-					tag.MergeAttribute(":class", b.GetClassForParent(context, valPath));
-					tag.RenderStart(context);
-					using (new ScopeContext(context, "btn", isBind.Path))
-					{
-						b.RenderMe(context, valPath);
-					}
-					tag.RenderEnd(context);
-				}
-				else
-				{
-					tag.MergeAttribute(":class", b.GetClassForParent(context, valPath));
+					if (valPath != null)
+						tag.MergeAttribute(":class", b.GetClassForParent(context, valPath));
 					tag.RenderStart(context);
 					b.RenderMe(context, valPath);
 					tag.RenderEnd(context);
 				}
 			}
-		}
-
-		void RenderDesription(RenderContext context)
-		{
-			new Separator().RenderElement(context);
-			var dBind = GetBinding(nameof(Description));
-			var wrap = new TagBuilder(null, "a2-tab-description");
-			wrap.RenderStart(context);
-			if (dBind != null)
+			else
 			{
-				var span = new TagBuilder("span");
-				span.MergeAttribute("v-text", dBind.GetPathFormat(context));
-				span.Render(context);
+				tag.MergeAttribute(":class", b.GetClassForParent(context, valPath));
+				tag.RenderStart(context);
+				b.RenderMe(context, valPath);
+				tag.RenderEnd(context);
 			}
-			else if (Description is UIElementBase uiDescr)
-				uiDescr.RenderElement(context);
-			else if (Description != null)
-				context.Writer.Write(context.LocalizeCheckApostrophe(Description.ToString()));
-			wrap.RenderEnd(context);
 		}
+	}
 
+	void RenderDesription(RenderContext context)
+	{
+		new Separator().RenderElement(context);
+		var dBind = GetBinding(nameof(Description));
+		var wrap = new TagBuilder(null, "a2-tab-description");
+		wrap.RenderStart(context);
+		if (dBind != null)
+		{
+			var span = new TagBuilder("span");
+			span.MergeAttribute("v-text", dBind.GetPathFormat(context));
+			span.Render(context);
+		}
+		else if (Description is UIElementBase uiDescr)
+			uiDescr.RenderElement(context);
+		else if (Description != null)
+			context.Writer.Write(context.LocalizeCheckApostrophe(Description.ToString()));
+		wrap.RenderEnd(context);
+	}
+
+	protected override void OnEndInit()
+	{
+		base.OnEndInit();
+		if (Description is XamlElement xamlElem)
+			xamlElem.SetParent(this);
 	}
 }
