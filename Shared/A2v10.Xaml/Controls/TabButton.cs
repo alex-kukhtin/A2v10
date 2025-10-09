00cf5fe9f70717516e6c3eea08c50aa28ca7f949
alex-kukhtin <alex.kukhtin@outlook.com>
2025-10-09 09:47:37 +0300

    tb-badge

--- Shared/A2v10.Xaml/Controls/TabButton.cs
+++ Shared/A2v10.Xaml/Controls/TabButton.cs
@@ -1,94 +1,94 @@
-﻿// Copyright © 2019 Alex Kukhtin. All rights reserved.
+﻿// Copyright © 2025 Oleksandr Kukhtin. All rights reserved.
 
 using System;
 using System.Collections.Generic;
 using System.Windows.Markup;
 
-namespace A2v10.Xaml
-{
-	public class TabButtonCollection : List<TabButton>
-	{
+namespace A2v10.Xaml;
 
+public class TabButtonCollection : List<TabButton>
+{
+
+}
+
+[ContentProperty("Content")]
+public class TabButton : UIElementBase
+{
+	public Object Content { get; set; }
+	public Object Description { get; set; }
+	public String ActiveValue { get; set; }
+    public String Badge { get; set; }
+
+    public override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
+	{
+		throw new NotImplementedException(nameof(RenderElement));
 	}
 
-	[ContentProperty("Content")]
-	public class TabButton : UIElementBase
+	private String GetActiveValueArg(RenderContext context)
 	{
-		public Object Content { get; set; }
-		public Object Description { get; set; }
+		var activeValueArg = $"'{ActiveValue}'";
+		var avBind = GetBinding(nameof(ActiveValue));
+		if (avBind != null)
+			activeValueArg = avBind.GetPathFormat(context);
+		return activeValueArg;
+	}
 
-		public String ActiveValue { get; set; }
+	public String GetClassForParent(RenderContext context, String valuePath)
+	{
+		var activeValueArg = GetActiveValueArg(context);
+		return $"{{'active': {activeValueArg} == {valuePath}}}";
+	}
 
-		public override void RenderElement(RenderContext context, Action<TagBuilder> onRender = null)
+	internal void RenderMe(RenderContext context, String valuePath)
+	{
+		if (SkipRender(context))
+			return;
+
+		var btn = new TagBuilder("a", "a2-tab-button");
+
+		var activeValueArg = GetActiveValueArg(context);
+
+		MergeAttributes(btn, context);
+		if (valuePath != null)
 		{
-			throw new NotImplementedException(nameof(RenderElement));
+			btn.MergeAttribute(":class", $"{{'active': {activeValueArg} == {valuePath}}}");
+			btn.MergeAttribute("@click.stop.prevent", $"$closeAllPopups(); {valuePath}={activeValueArg}");
 		}
 
-		private String GetActiveValueArg(RenderContext context)
-		{
-			var activeValueArg = $"'{ActiveValue}'";
-			var avBind = GetBinding(nameof(ActiveValue));
-			if (avBind != null)
-				activeValueArg = avBind.GetPathFormat(context);
-			return activeValueArg;
-		}
+		btn.RenderStart(context);
+		RenderContent(context);
+        RenderBadge(context, Badge);
+        RenderDescription(context);
+		btn.RenderEnd(context);
+	}
 
-		public String GetClassForParent(RenderContext context, String valuePath)
-		{
-			var activeValueArg = GetActiveValueArg(context);
-			return $"{{'active': {activeValueArg} == {valuePath}}}";
-		}
+	void RenderContent(RenderContext context)
+	{
+		var span = new TagBuilder("span", "content");
+		var cntBind = GetBinding(nameof(Content));
+		if (cntBind != null)
+			span.MergeAttribute("v-text", cntBind.GetPathFormat(context));
+		span.RenderStart(context);
+		if (Content is UIElementBase contUi)
+			contUi.RenderElement(context);
+		else if (Content != null)
+			context.Writer.Write(context.LocalizeCheckApostrophe(Content.ToString()));
+		span.RenderEnd(context);
+	}
 
-		internal void RenderMe(RenderContext context, String valuePath)
-		{
-			if (SkipRender(context))
-				return;
-
-			var btn = new TagBuilder("a", "a2-tab-button");
-
-			var activeValueArg = GetActiveValueArg(context);
-
-			MergeAttributes(btn, context);
-			if (valuePath != null)
-			{
-				btn.MergeAttribute(":class", $"{{'active': {activeValueArg} == {valuePath}}}");
-				btn.MergeAttribute("@click.stop.prevent", $"$closeAllPopups(); {valuePath}={activeValueArg}");
-			}
-
-			btn.RenderStart(context);
-			RenderContent(context);
-			RenderDescription(context);
-			btn.RenderEnd(context);
-		}
-
-		void RenderContent(RenderContext context)
-		{
-			var span = new TagBuilder("span", "content");
-			var cntBind = GetBinding(nameof(Content));
-			if (cntBind != null)
-				span.MergeAttribute("v-text", cntBind.GetPathFormat(context));
-			span.RenderStart(context);
-			if (Content is UIElementBase contUi)
-				contUi.RenderElement(context);
-			else if (Content != null)
-				context.Writer.Write(context.LocalizeCheckApostrophe(Content.ToString()));
-			span.RenderEnd(context);
-		}
-
-		void RenderDescription(RenderContext context)
-		{
-			var descBind = GetBinding(nameof(Description));
-			if (descBind == null && Description == null)
-				return;
-			var span = new TagBuilder("span", "description");
-			if (descBind != null)
-				span.MergeAttribute("v-text", descBind.GetPathFormat(context));
-			span.RenderStart(context);
-			if (Description is UIElementBase descUi)
-				descUi.RenderElement(context);
-			else if (Description != null)
-				context.Writer.Write(context.LocalizeCheckApostrophe(Description.ToString()));
-			span.RenderEnd(context);
-		}
+	void RenderDescription(RenderContext context)
+	{
+		var descBind = GetBinding(nameof(Description));
+		if (descBind == null && Description == null)
+			return;
+		var span = new TagBuilder("span", "description");
+		if (descBind != null)
+			span.MergeAttribute("v-text", descBind.GetPathFormat(context));
+		span.RenderStart(context);
+		if (Description is UIElementBase descUi)
+			descUi.RenderElement(context);
+		else if (Description != null)
+			context.Writer.Write(context.LocalizeCheckApostrophe(Description.ToString()));
+		span.RenderEnd(context);
 	}
 }
